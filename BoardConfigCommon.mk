3d8e930fc6eb0f4cd83518d9f53b523a9817ff8e
Bruno Martins <bgcngm@gmail.com>
2020-11-20 20:12:15 +0000

    sm8250-common: Remove deprecated telephony flag
    The custom jar is dead for good in Android 11.
    
    Change-Id: I7f0f7afd186b00f01183bf4479092d03897e074a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,9 +172,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2022-10-01
 
-# Telephony
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
-
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
